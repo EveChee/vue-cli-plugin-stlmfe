b566bf3c3548ce0b4d4fc5660e9531c7dedda40c
EveChee <evec@vip.qq.com>
2020-05-19 16:20:06 +0800

    fix typescript generator

--- generator.js
+++ generator.js
@@ -5,17 +5,17 @@ module.exports = api => {
 
   api.render({
     [api.entryFile]: './template/src/main.js',
-    './src/set-public-path.js': './template/src/set-public-path.js',
-    './src/single-spa-vue-mfe.js': isTs ? './template/src/single-spa-vue-mfe.ts' : './template/src/single-spa-vue-mfe.js',
+    [`./src/set-public-path.${isTs ? 'ts' : 'js'}`]: './template/src/set-public-path.js',
+    [`./src/single-spa-vue-mfe.${isTs ? 'ts' : 'js'}`]: isTs ? './template/src/single-spa-vue-mfe.ts' : './template/src/single-spa-vue-mfe.js',
   }, {
-    isTs,
-    usesRouter,
-    appName: projectName,
-  });
+  isTs,
+  usesRouter,
+  appName: projectName,
+});
 
-  api.extendPackage({
-    dependencies: {
-      'systemjs-webpack-interop': '^1.1.0',
-    }
-  })
+api.extendPackage({
+  dependencies: {
+    'systemjs-webpack-interop': '^1.1.0',
+  }
+})
 }
