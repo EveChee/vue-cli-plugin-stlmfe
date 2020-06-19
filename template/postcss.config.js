1d3f62bd28748ee517ac1e67ca5d01845ce91d30
EveChee <evec@vip.qq.com>
2020-06-19 13:47:41 +0800

    修改postcss.config模板 自动读取项目名

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -1,7 +1,8 @@
+const APP_NAME = require('package.json').name
 module.exports = () => {
   return {
     plugins:[
-      require('postcss-plugin-namespace')('#<%=appName%>', { ignore: [ /^\.el-/ , '#<%=appName%>'] }),
+      require('postcss-plugin-namespace')(`#${APP_NAME}`, { ignore: [ /^\.el-/ , `#${APP_NAME}`] }),
       require('autoprefixer')
     ],
   }
